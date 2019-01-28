1d449dd2eb5e84d41d8d37aa6fc8f318cbe12f51
Nick <nickvandettethree@gmail.com>
2019-01-28 07:44:30 -0500

    working on adding google support

--- bot.py
+++ bot.py
@@ -1,49 +1,70 @@
 import discord
 import string
 import requests, re
-from pytineye import TinEyeAPIRequest
 
-searchLocation = 'https://www.tineye.com/search'
-resultRegex = re.compile('\d{1,3}(,\d{3})*(\.\d+)? results?')
-urlRegex = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})'
-pic_ext = ['.jpg','.png','.jpeg']
+TINEYE_LOCATION = 'https://www.tineye.com/search'
+GOOGLE_LOCATION = 'https://www.google.com/searchbyimage?image_url={}&encoded_image=&image_content=&filename=&hl=en'
+RESULT_REGEX = re.compile('\d{1,3}(,\d{3})*(\.\d+)? results?')
+URL_REGEX = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})'
+# If message 'endswith', it will consides it an attachment
+PIC_EXT = ['.jpg','.png','.jpeg']
+
 client = discord.Client()
-
-with open('../TOKEN') as tokenFile:
-    TOKEN = tokenFile.readline()
 
 async def send(location, message):
     global client
     await client.send_message(location, message)
 
 async def findDuplicates(url, message):
-    response = requests.post(searchLocation, data = {'url': url})
-    lines = response.text.split('\n')
-    for line in lines:
-        if re.match(resultRegex, line):
-            print(line)
-            numResults = int(re.sub('\D', '', line))
-            if numResults:
-                await send(message.channel, '**{}** other instance(s) of this image have been found on the internet.\nThey can be found here: {}'.format(numResults, response.url))
-            else:
-                await client.add_reaction(message, '\u2611')
-            break
+    # # Perform request to tineye service
+    # response = requests.post(TINEYE_LOCATION, data = {'url': url})
+    # lines = response.text.split('\n')
+    # numResults = 0
+    # for line in lines:
+    #     if re.match(resultRegex, line): # Parse number of results
+    #         numResults += int(re.sub('\D', '', line))
+    #         break
+    
+    print("performing on {}".format(url))
+    # Google reverse image search
 
+    searchUrl = 'https://www.google.com/searchbyimage?image_url={}'.format(url)
+    response = requests.get(searchUrl, allow_redirects=False)
+    print("first response")
+    print(response.text)
+    fetchUrl = response.headers['Location']
+    print("fetch")
+    print(fetchUrl)
+    response = requests.get(fetchUrl)
+    print("second response")
+    print(response.text)
+
+    # if numResults:
+    #     await send(message.channel, 'Other instance(s) of this image have been found on the internet.\nThey can be found here: {}'.format(numResults, response.url))
+    # else:
+    #     await client.add_reaction(message, '\u2611')
+    
 @client.event
 async def on_message(message):
     if message.attachments:
         imageUrl = message.attachments[0]['url']
         await findDuplicates(imageUrl, message)
     else:
-        for ext in pic_ext:
+        # Message still might be an image
+        for ext in PIC_EXT:
             if message.content.endswith(ext):
-                match = re.match(urlRegex, message.content)
+                match = re.match(URL_REGEX, message.content)
                 if match:
+                    # Message now definitely contains an image url
                     imageUrl = match.group(1)
+                    print(imageUrl)
                     await findDuplicates(imageUrl, message)
 
 @client.event
 async def on_ready():
     print('Logged in!')
-        
+
+with open('../TOKEN') as tokenFile:
+    TOKEN = tokenFile.readline()
+
 client.run(TOKEN)
