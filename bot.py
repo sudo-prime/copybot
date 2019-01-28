eeb38f66e11c357cccc672bc84339b251596b8cd
Nick VanDette <nicholas.vandette@nuix.com>
2019-01-28 14:36:41 -0500

    added google reverse image search functionality

--- bot.py
+++ bot.py
@@ -1,48 +1,57 @@
+from pyshorteners import Shortener
 import discord
 import string
-import requests, re
+import requests
+import json
+import re
+
+with open('../KEY') as apiKey:
+    KEY = apiKey.readline()
 
 TINEYE_LOCATION = 'https://www.tineye.com/search'
 GOOGLE_LOCATION = 'https://www.google.com/searchbyimage?image_url={}&encoded_image=&image_content=&filename=&hl=en'
+LINK_SHORTENER_LOCATION = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(KEY)
 RESULT_REGEX = re.compile('\d{1,3}(,\d{3})*(\.\d+)? results?')
+MATCHING_REGEX = re.compile('Pages that include matching images')
 URL_REGEX = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})'
-# If message 'endswith', it will consides it an attachment
-PIC_EXT = ['.jpg','.png','.jpeg']
+PIC_EXT = ['.jpg','.png','.jpeg'] # If message 'endswith', it will considers it an attachment
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0'}
 
 client = discord.Client()
+shortener = Shortener('Tinyurl')
 
 async def send(location, message):
     global client
-    await client.send_message(location, message)
+    embed = discord.Embed(title="", description=message)
+    await client.send_message(location, embed=embed)
+
+def shorten(link):
+    return shortener.short(link)
 
 async def findDuplicates(url, message):
-    # # Perform request to tineye service
-    # response = requests.post(TINEYE_LOCATION, data = {'url': url})
-    # lines = response.text.split('\n')
-    # numResults = 0
-    # for line in lines:
-    #     if re.match(resultRegex, line): # Parse number of results
-    #         numResults += int(re.sub('\D', '', line))
-    #         break
-    
-    print("performing on {}".format(url))
+    print('Performing reverse image search on ' + url + '...')
+    results = []
+
+    # Tineye reverse image search
+    response = requests.post(TINEYE_LOCATION, data={'url': url})
+    lines = response.text.split('\n')
+    for line in lines:
+        if re.match(RESULT_REGEX, line):
+            if int(re.sub('\D', '', line)):
+                results.append('Tineye: ' + shorten(response.url))
+                break
+
     # Google reverse image search
-
     searchUrl = 'https://www.google.com/searchbyimage?image_url={}'.format(url)
-    response = requests.get(searchUrl, allow_redirects=False)
-    print("first response")
-    print(response.text)
-    fetchUrl = response.headers['Location']
-    print("fetch")
-    print(fetchUrl)
-    response = requests.get(fetchUrl)
-    print("second response")
-    print(response.text)
+    response = requests.get(searchUrl, allow_redirects=True, headers=HEADERS)
+    result = re.search(MATCHING_REGEX, response.text)
+    if result: results.append('Google: ' + shorten(response.url))
 
-    # if numResults:
-    #     await send(message.channel, 'Other instance(s) of this image have been found on the internet.\nThey can be found here: {}'.format(numResults, response.url))
-    # else:
-    #     await client.add_reaction(message, '\u2611')
+    if results:
+        await send(message.channel, 'Other instances of this image have been found on the internet.\nThey can be found here: \n{}'.format('\n'.join(results)))
+    else:
+        await client.add_reaction(message, '\u2611')
+    print('Done!')
     
 @client.event
 async def on_message(message):
@@ -57,7 +66,6 @@ async def on_message(message):
                 if match:
                     # Message now definitely contains an image url
                     imageUrl = match.group(1)
-                    print(imageUrl)
                     await findDuplicates(imageUrl, message)
 
 @client.event
