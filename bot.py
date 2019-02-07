6fc4250fda3ef513d9c209ba31c78b93fa52960d
Nick VanDette <nicholas.vandette@nuix.com>
2019-02-07 14:19:32 -0500

    small refactors

--- bot.py
+++ bot.py
@@ -5,12 +5,8 @@ import discord
 import json
 import re
 
-with open('../KEY') as apiKey:
-    KEY = apiKey.readline()
-
 TINEYE_LOCATION = 'https://www.tineye.com/search'
 GOOGLE_LOCATION = 'https://www.google.com/searchbyimage?image_url={}'
-LINK_SHORTENER_LOCATION = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(KEY)
 RESULT_REGEX = re.compile('\d{1,3}(,\d{3})*(\.\d+)? results?')
 MATCHING_REGEX = re.compile('Pages that include matching images')
 URL_REGEX = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})'
@@ -19,15 +15,13 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/201
 
 client = discord.Client()
 shortener = Shortener('Tinyurl')
-threads = []
 
-async def send(location, message):
-    global client
+async def sendMessage(location, message):
     embed = discord.Embed(title="", description=message)
     await client.send_message(location, embed=embed)
 
 async def findDuplicates(imageUrl, message):
-    print('Performing reverse image search on ' + imageUrl + '...')
+    logging.debug('Searching for %s' % message.author)
     results = []
 
     # Tineye reverse image search
@@ -41,13 +35,15 @@ async def findDuplicates(imageUrl, message):
 
     # Google reverse image search
     searchUrl = 'https://www.google.com/searchbyimage?image_url={}'.format(imageUrl)
-
     async with aiohttp.get(searchUrl, allow_redirects=True, headers=HEADERS) as response:
         result = re.search(MATCHING_REGEX, await response.text())
         if result != None: results.append('Google: ' + shortener.short(response.url))
 
     if results:
-        await send(message.channel, 'Other instances of this image have been found on the internet.\nThey can be found here: \n{}'.format('\n'.join(results)))
+        await sendMessage(
+            message.channel,
+            'Other instances of this image have been found on the internet.\nThey can be found here:\n{}'
+                .format('\n'.join(results)))
     else:
         await client.add_reaction(message, '\u2611')
     
